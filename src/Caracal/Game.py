1f2df8889cc6e7e5edbab21248ab6aa421f57e08
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-03-22 22:35:49 +0300

    optimize: scene drawing

--- src/Caracal/Game.py
+++ src/Caracal/Game.py
@@ -3,23 +3,25 @@ import coloredlogs, logging
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='DEBUG', logger=logger)
 from functools import lru_cache
+
 class Game:
-    def __init__(self, window_name="Caracal Window", x=400,y=640):
+    def __init__(self, window_name="Caracal Window", width=400, height=640):
         import pygame
         self.pygame = pygame
         self.window = pygame.display
         self.window_name = window_name
-        self.x = x
-        self.y = y
+        self.SC_WIDTH = width
+        self.SC_HEIGHT = height
         self.before_update=[]
         self.after_update=[]
         self.during_input=[]
         self.inputs=[]
-        self.surface = None
+        self.screen = None
+        self.surface = pygame.Surface((width, height))
         self.Scene = None
         self.clock = pygame.time.Clock()
-        self.max_fps = 120
-    
+        self.max_fps = 0
+        self._lazy_loads = []
 
     def preflip_tasks(self):
         #accepts functions as tasks to be handled BEFORE updating frames, in a list.
@@ -37,63 +39,64 @@ class Game:
             task()
 
     def instantiate(self, Object):
-        self.before_update.append(lambda: self.window.blit(Object.sprite, (Object.x, Object.y)))
+        self.before_update.append(lambda: self.screen.blit(Object.sprite, (Object.x, Object.y)))
         self.before_update.append(Object.update)
         self.during_input.append(Object.input_update)
 
-    def draw_scene(self, Scene):
-        for tile in self.Scene.tiles:
-                    self.surface.blit(tile[0], (self.Scene.camera_x+tile[1], self.Scene.camera_y+tile[2]))
-    
+    def _init_window(self):
+        logger.info("Initializing window...")
+        self.screen = self.window.set_mode((self.SC_WIDTH,self.SC_HEIGHT))
+        self.after_update.append(lambda: self.window.set_caption(f"{self.window_name} - FPS: {self.fps:.2f} - dt: {self.dt:.2f}"))
+        logger.info(f"Starting Load Tasks...")
+        for task in self._lazy_loads:
+            task[0](*task[1:])
+
     @lru_cache
     def initialize_scene(self, Scene):
-        
         Scene.tiles.clear()
         Scene.calculate()
-        for tile in Scene.tiles:
-            tile[0] = self.pygame.image.load(tile[0])
         self.Scene = Scene
-
-                            #(texture, x-axis, y-axis.)
-        self.before_update.append(lambda: self.window.set_caption(f"{self.window_name} - FPS: {self.fps:.2f} - dt: {self.dt:.2f}"))
-
-        print("in")
+        self.before_update.append(lambda: self.screen.blit(self.surface, (Scene.camera_x, Scene.camera_y)))
+        for tile, iso_x, iso_y in self.Scene.tiles:
+            x = self.Scene.camera_x + iso_x
+            y = self.Scene.camera_y + iso_y
+            self.surface.blit(Scene.texture[tile], (x, y))
 
     def run(self):
         Thread(target=self.run_func).start()
 
     def run_func(self):
         pygame = self.pygame
-        self.surface = self.window.set_mode((self.x,self.y))
+        self._init_window()
         self.window.set_caption(self.window_name)
         logger.info("Pygame thread started.")
+        if self.Scene is not None:
+            self.initialize_scene(self.Scene)
+            self.draw_scene(self.Scene)
+        logger.info("Ok")
         while True:
+            self.screen.fill((0,0,0))
             self.preflip_tasks()
             self.dt = self.clock.tick(self.max_fps)
             self.fps = self.clock.get_fps()
             pygame.display.update()
-            self.surface.fill((0, 0, 0))
 
-            
             self.inputs = pygame.event.get()
             pressed = pygame.key.get_pressed()
-
-            if self.Scene is not None:
-                #Run separately from input loop to avoid being called only on key press. surface.fill may overwrite scene unless user is quick.
-                self.initialize_scene(self.Scene)
-                self.draw_scene(self.Scene)
-                
 
             for input in self.inputs:
                 if self.Scene is not None:
                     self.Scene.movement_control(pressed)
                 else:
-                    print(type(self.Scene))
+                    pass
                 self.input_tasks()
                 
                 if input.type == pygame.QUIT:
                     return
+                if input.type == pygame.KEYDOWN:
+                    if input.key == pygame.K_F1:
+                        self.Scene = self.initialize_scene(self.Scene)
+                        self.draw_scene(self.Scene)
             
-
-
+            self.postflip_tasks()  # not sure if this is the currect place for this 
 
--- src/Caracal/Mapping/Isometrics.py
+++ src/Caracal/Mapping/Isometrics.py
@@ -1,11 +1,12 @@
-
+import typing
 
 
 class IsometricScene:
-    def __init__(self, map, tile_width, tile_height, texture):
+    def __init__(self, app, map, tile_width, tile_height, texture):
+        self.app = app
         import pygame
         self.pygame = pygame
-        self.map = map
+        self.map: typing.List[int, int, int] = map
         self.tile_width = tile_width
         self.tile_height = tile_height
         self.tile_half_width = tile_width / 2
@@ -14,8 +15,17 @@ class IsometricScene:
         self.texture = texture
         self.camera_x=0
         self.camera_y=0
+        self._load_texture_lazy()
         self.calculate()
 
+    def _load_texture_lazy(self, keep_alpha=True):
+        if self.pygame.display.get_init():
+            for i, tex_path in enumerate(self.texture):
+                img = self.pygame.image.load(tex_path)
+                img = img.convert_alpha() if keep_alpha else img.convert()
+                self.texture[i] = img
+        else:
+            self.app._lazy_loads.append((self._load_texture_lazy, keep_alpha))
 
     def movement_control(self, pressed):
         pygame = self.pygame
@@ -28,19 +38,13 @@ class IsometricScene:
         if pressed[pygame.K_d]:
             self.camera_x -= 10
 
-    def calculate(self):
+    def calculate(self):  # TODO: There shouldnt be both self.tiles and self.map, there should only be one
         #method created to reduce the load on the initialization method.
         pygame = self.pygame
         for row in range(len(self.map)):
             for col in range(len(self.map[row])):
-                # print(self.texture)
                 # Calculate the position of the tile in isometric coordinates
                 iso_x = (col - row) * self.tile_half_width/2
                 iso_y = (col + row) * self.tile_half_height/4
                 # Save the position of the tile as a tuple
-                self.tiles.append([(self.texture[self.map[row][col]]), iso_x, iso_y])
-    
-
-      
-
-
+                self.tiles.append([self.map[row][col], iso_x, iso_y])  # maybe only save just the tile, not the iso pos?
