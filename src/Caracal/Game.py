3227777aee68fd472f3e61f3944cb594403d80c9
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-04-30 23:32:26 +0300

    WIP: change main loop structure

--- src/Caracal/Game.py
+++ src/Caracal/Game.py
@@ -19,85 +19,61 @@ class Game:
     def __init__(self, window_name="Caracal Window", width=400, height=640):
         self.window_name = window_name
         self.SC_WIDTH, self.SC_HEIGHT = (width, height)
-        self.ui_queue = []
-        self.before_update = []
-        self.after_update = []
-        self.during_input = []
-        self.inputs = []
+        self.clock = pygame.time.Clock()
         self.screen = None
         self.Scene = None
-        self.clock = pygame.time.Clock()
         self.max_fps = 120  # 0
         self._lazy_loads = []
         self._last_update_time = time.time()
+        self._define_tasks()
 
         if os.path.exists(".caracal"):
             pass
         else:
             logger.info("Creating missing .caracal directory...")
             os.mkdir(".caracal")
+    def _define_tasks(self):
+        self.draw_tasks = []  # TODO: np arrays
+        self.update_tasks = []
+        self.input_tasks = []
 
-    def preflip_tasks(self):
-        # accepts functions as tasks to be handled BEFORE updating frames, in a list.
-        for task in self.before_update:
-            print(str(task))
-            task()
-
-    def postflip_tasks(self):
-        # accepts functions as tasks to be handled AFTER updating frames, in a list.
-        for task in self.after_update:
-            task()
-
-    def input_tasks(self, pressed, pygame):
-        # accepts functions as tasks to be handled DURING the pygame event loop.
-        for task in self.during_input:
-            task(pressed)
-
-    def ui_tasks(self):
-        # accepts functions as tasks to be drawn LAST amongst all other blits and serves the purpose of interactable ui.
-        for task in self.ui_queue:
-            task()
-
-    def instantiate(self, Object, under_scene=None):
+    def instantiate(self, Object, gamestate, under_scene=None, update_task=None, input_task=None):
         """
         usage: Game.instantiate(Object)
 
-        Alternatively, you can choose to make the object a child of the scene.
-        This makes it correspond its position to the scenes camera, essentially
+        Alternatively, you can choose to make they object a child of the scene.
+        This makes it correspond its position to the scenes camera, essentiall
         it synchronizes the map tiles with the Objects.
         """
         if under_scene is not None:
-            self.before_update.append(lambda: self.screen.blit(
-                Object.sprite, (-self.Scene.camera_x + Object.x, -self.Scene.camera_y + Object.y)))
-            self.before_update.append(Object.update)
-            self.during_input.append(Object.input_update)
+            _task = lambda: self.screen.blit(
+                Object.sprite, (-self.Scene.camera_x + Object.x, -self.Scene.camera_y + Object.y))
         else:
-            self.before_update.append(lambda: self.screen.blit(
-                Object.sprite, (Object.x, Object.y)))
-            self.before_update.append(Object.update)
-            self.during_input.append(Object.input_update)
+            _task = lambda: self.screen.blit(
+                Object.sprite, (Object.x, Object.y))
+        self.draw_tasks.append({"task": _task, "gamestate": gamestate})
+        if update_task: self.update_tasks.append({"task": update_task, "gamestate":gamestate})
+        if input_task: self.input_tasks.append({"task":input_task, "gamestate":gamestate})
 
-    def _init_window(self):
-        logger.info("Initializing window...")
-        self.screen = pygame.display.set_mode((self.SC_WIDTH, self.SC_HEIGHT))
-        logger.info("Initializing ui...")
-        self.after_update.append(lambda: pygame.display.set_caption(
-            f"{self.window_name} - FPS: {self.fps:.2f} - dt: {self.dt:.2f}"))
+    def _init_window(self, gamestate, FLAGS=None):
+        logger.info("Initializing...")
+        self.screen = pygame.display.set_mode((self.SC_WIDTH, self.SC_HEIGHT), flags=FLAGS)
+        _task = lambda: pygame.display.set_caption(f"{self.window_name} - FPS: {self.fps:.2f} - dt: {self.dt:.2f}")
+        self.update_tasks.append({"task":_task, "gamestate":gamestate})
         logger.info(f"Starting Load Tasks...")
         for task in self._lazy_loads:
             task[0](*task[1:])
 
     @lru_cache
-    def initialize_scene(self, Scene):
+    def initialize_scene(self, Scene, gamestate):
         self.Scene = Scene
-        # self.Scene.cache_surface()
-        self.before_update.append(
-            lambda: self.screen.blit(self.Scene.surface, (-self.Scene.camera_x, -self.Scene.camera_y)))
+        _task = lambda: self.screen.blit(self.Scene.surface, (-self.Scene.camera_x, -self.Scene.camera_y))
+        self.draw_tasks.append({"task":_task, "gamestate":gamestate})
 
-    def initialize_text(self, text):
-        self.ui_queue.append(
-            # given screen and not surface because it is supposed to be higher than scene in the hierarchy.
-            lambda: self.screen.blit(text.text, text.text_rect))
+    def initialize_text(self, text, gamestate):
+        # given screen and not surface because it is supposed to be higher than scene in the hierarchy.
+        _task = lambda: self.screen.blit(text.text, text.text_rect)
+        self.ui_queue.append({"task":_task, "gamestate":gamestate})
 
     def initialize_button(self, button):
         self.ui_queue.append(
@@ -120,9 +96,7 @@ class Game:
         Thread(target=self.run_func).start()
 
     def run_func(self):  # TODO: simplify and clean up this function.
-        pygame.display.set_caption(self.window_name)
-        logger.info("Pygame thread started.")
-        logger.info("OK")
+        logger.info("Pygame thread started. OK.")
         while True:
             self.dt = self.clock.tick(self.max_fps)
             self.fps = self.clock.get_fps()
@@ -131,18 +105,16 @@ class Game:
             self.draw_scene(self.Scene.camera)
             self.ui_tasks()
             pygame.display.update()
-
-            self.inputs = pygame.event.get()
             self.pressed = pygame.key.get_pressed()
 
             current_time = time.time()
             elapsed_time = current_time - self._last_update_time
             # seperate input checks, otherwise you will have to time the window exit.
-            for input in self.inputs:
-                if input.type == pygame.QUIT:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     return
-                if input.type == pygame.KEYDOWN:
-                    if input.key == pygame.K_F1:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_F1:
                         # will be useful soon
                         pass
 
@@ -156,3 +128,23 @@ class Game:
                 self._last_update_time = time.time()
 
             self.postflip_tasks()  # not sure if this is the currect place for this
+
+    def run_func(self):
+        logger.info("Pygame thread started. OK.")
+        while True:
+            self.input()
+            self.update()
+            self.dt = self.clock.tick(self.max_fps)
+            self.dt /= 1000  # convert to seconds
+            self.draw()
+    
+    def draw(self):
+        for data in self.draw_tasks:
+            state_to_draw = data["state"]
+            _task = data["task"]
+    
+    def update(self):
+        pass
+
+    def input(self):
+        pass
